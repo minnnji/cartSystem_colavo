b472afc3ddae35d54b606d26f7e2d099d70a4af4
minnnji <choimj2326@gmail.com>
2020-05-31 22:35:47 +0900

    Feat: getData by axios

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const requestItem = async () => {
+  try {
+    const { data } = await axios.get(
+      'https://us-central1-colavolab.cloudfunctions.net/requestAssignmentCalculatorData'
+    );
+
+    return data.items;
+  } catch (err) {
+    alert(err);
+  }
+};
+
+export const requestDiscounts = async () => {
+  try {
+    const { data } = await axios.get(
+      'https://us-central1-colavolab.cloudfunctions.net/requestAssignmentCalculatorData'
+    );
+
+    return data.discounts;
+  } catch (err) {
+    alert(err);
+  }
+};
