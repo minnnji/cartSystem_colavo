44401f173bdacda4b80e4ed993aa8bdb691b21b4
minnnji <choimj2326@gmail.com>
2020-06-02 01:09:46 +0900

    Feat: handle selected discounts with recoil store

--- src/components/DiscountList.tsx
+++ src/components/DiscountList.tsx
@@ -1,20 +1,93 @@
 import React from 'react';
 import Header from './layout/Header';
+import CheckBox from './layout/Checkbox.js';
+import styled from 'styled-components';
+import theme from './layout/theme';
 
 interface DiscountListProps {
-  discount: object;
+  isLoading: boolean;
+  discount: [
+    string,
+    {
+      name: string;
+      rate: number;
+    }
+  ][];
+  selectedDiscountList: object[];
+  handleSelectedDiscountList: (
+    item: [
+      string,
+      {
+        name: string;
+        rate: number;
+      }
+    ]
+  ) => void;
   handleBack: () => void;
 }
 
 const DiscountList = (props: DiscountListProps) => {
-  const { discount, handleBack } = props;
-  console.log(discount);
+  const {
+    isLoading,
+    discount,
+    selectedDiscountList,
+    handleSelectedDiscountList,
+    handleBack
+  } = props;
+
+  const isSelectedDiscount = (key: string) =>
+    selectedDiscountList.some(selectedDiscount => selectedDiscount[0] === key);
+
+  const discounts = discount.map(item => {
+    return (
+      <Li key={item[0]}>
+        <CheckBox
+          checked={isSelectedDiscount(item[0])}
+          onChange={() => handleSelectedDiscountList(item)}
+        />
+        <Info>
+          <Name>{item[1].name}</Name>
+          <Price>{Math.floor(item[1].rate * 100)}% 할인</Price>
+        </Info>
+      </Li>
+    );
+  });
 
   return (
     <>
       <Header title='할인 추가하기' handleBack={handleBack} />
+      <Section>
+        {isLoading ? <div>Loading...</div> : <ul>{discounts}</ul>}
+      </Section>
     </>
   );
 };
+
+const Section = styled.section`
+  padding: 16px;
+`;
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
+  border-top: 1px solid lightgray;
+`;
+
+const Info = styled.dl`
+  display: inline-block;
+  margin-right: 10px;
+  font-size: 18px;
+`;
+
+const Name = styled.dt`
+  margin-bottom: 10px;
+`;
+
+const Price = styled.dd`
+  font-weight: 200;
+  font-size: 16px;
+  color: ${theme.COLOR_PINK_2};
+`;
 
 export default DiscountList;
--- src/containers/DiscountListContainer.tsx
+++ src/containers/DiscountListContainer.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { cartDiscountState } from '../store/atoms';
 import DiscountList from '../components/DiscountList';
 import { requestDiscounts } from '../api';
 
@@ -9,22 +11,58 @@ interface DiscountContainerProps {
 
 const DiscountContainer = (props: DiscountContainerProps) => {
   const { history } = props;
-  const [discount, setDiscount] = useState({});
+  const [selectedDiscountList, setSelectedDiscountList] = useRecoilState(
+    cartDiscountState
+  );
 
-  const handleBack = () => {
-    history.goBack();
-  };
+  const [discount, setDiscount] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getDiscount = async () => {
-      const data = await requestDiscounts();
-      setDiscount(data);
+      setIsLoading(true);
+
+      const data: object = await requestDiscounts();
+      const items: [string, object][] = [];
+
+      for (let key in data) items.push([key, data[key]]);
+      setDiscount(items);
+
+      setIsLoading(false);
     };
 
     getDiscount();
   }, []);
 
-  return <DiscountList discount={discount} handleBack={handleBack} />;
+  const handleBack = () => {
+    history.goBack();
+  };
+
+  const handleSelectedDiscountList = (
+    item: [
+      string,
+      {
+        name: string;
+        rate: number;
+      }
+    ]
+  ) => {
+    const index = selectedDiscountList.map(item => item[0]).indexOf(item[0]);
+    const newItemList = [...selectedDiscountList];
+
+    index === -1 ? newItemList.push(item) : newItemList.splice(index, 1);
+    setSelectedDiscountList(newItemList);
+  };
+
+  return (
+    <DiscountList
+      isLoading={isLoading}
+      discount={discount}
+      selectedDiscountList={selectedDiscountList}
+      handleSelectedDiscountList={handleSelectedDiscountList}
+      handleBack={handleBack}
+    />
+  );
 };
 
 export default DiscountContainer;
